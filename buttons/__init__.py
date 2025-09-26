5a9f88bd2975f316b037e43954f9e3855d3d0f81
Mirjahon <mirjahonqochqorov@gmail.com>
2025-09-26 15:55:06 +0500

    buigunga yetadi

--- buttons/__init__.py
+++ buttons/__init__.py
@@ -1,2 +1,1 @@
 from .user_text import *
-from .admin_text import *
--- handler/user.py
+++ handler/user.py
@@ -1,11 +1,20 @@
-from aiogram.types import Message
-from aiogram import router
-from aiogram.filters import CommandStart,Command
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from buttons import REG_TEXT
 
-user_router = router()
-
+user_router = Router()
 
 @user_router.message(CommandStart())
 async def start(message: Message):
-    await message.answer(text)
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📝 Registration")]
+        ],
+        resize_keyboard=True
+    )
+    await message.answer(REG_TEXT, reply_markup=keyboard)
+
+@user_router.message(lambda msg: msg.text == "📝 Registration")
+async def registration(message: Message):
+    await message.answer("Siz ro‘yxatdan o‘tish jarayonini boshladingiz ✅\nIltimos, ismingizni yuboring.")
